cb26454b0d5e2bbc0b38f708bda3c4c5900ae10c
Exeton <benjamin.diederich@gmail.com>
2019-01-05 13:07:55 -0500

    Fixed bug where changing worlds created an error in the console.

--- src/online/fireflower/server_tours/TrailDisplayerRunnable.java
+++ src/online/fireflower/server_tours/TrailDisplayerRunnable.java
@@ -2,6 +2,7 @@ package online.fireflower.server_tours;
 
 import online.fireflower.server_tours.Particles.ParticleEffects;
 import org.bukkit.Location;
+import org.bukkit.entity.Player;
 
 import java.util.List;
 
@@ -16,8 +17,27 @@ public class TrailDisplayerRunnable implements Runnable {
     @Override
     public void run() {
         for(ServerTourInfo serverTourInfo : serverTours)
-            for (Location location : serverTourInfo.particleLocatons)
-                if (serverTourInfo.getActivePlayers().size() > 0)
-                    serverTourInfo.particleType.display(0, 0, 0, 0F, 3, location, serverTourInfo.getActivePlayers());
+            for (Location location : serverTourInfo.particleLocatons){
+
+                try{
+                    sendParticlesForLocation(location, serverTourInfo);
+                }catch (Exception e){
+
+                    List<Player> activePlayers = serverTourInfo.getActivePlayers();
+                    for (int i = activePlayers.size() - 1; i > -1; i--){
+                        Player player = activePlayers.get(i);
+                        if (!player.isOnline() || !player.getWorld().equals(location.getWorld()))
+                            activePlayers.remove(player);
+                    }
+
+                    sendParticlesForLocation(location, serverTourInfo);
+                }
+            }
+    }
+
+    private void sendParticlesForLocation(Location location, ServerTourInfo serverTourInfo){
+        if (serverTourInfo.getActivePlayers().size() > 0)
+            serverTourInfo.particleType.display(0, 0, 0, 0F, 3, location, serverTourInfo.getActivePlayers());
+
     }
 }
